991d8e2ad4cee7f218b022e8529db0354d1d5530
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2024-11-03 22:31:25 +0700

    cập nhập button trong view tim kiếm

--- src/main/java/view/TimKiem_View.java
+++ src/main/java/view/TimKiem_View.java
@@ -20,7 +20,7 @@ public class TimKiem_View extends JFrame {
 	private JPanel contentPane;
 	private JLabel lblNewLabel;
 	private RoundButton btn_DangNhap;
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,7 +33,7 @@ public class TimKiem_View extends JFrame {
 			}
 		});
 	}
-	
+
 	public TimKiem_View() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1250, 800);
@@ -43,17 +43,18 @@ public class TimKiem_View extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		lblNewLabel = new JLabel("Đây là trang tìm kiếm chuyến tàu");
 		lblNewLabel.setBounds(435, 10, 280, 22);
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 18));
 		contentPane.add(lblNewLabel);
-		
-		btn_DangNhap = new RoundButton("Đăng Nhập", ColorConstants.PRIMARY_COLOR, ColorConstants.HOVER_COLOR);
+
+		btn_DangNhap = new RoundButton("Đăng Nhập");
 		btn_DangNhap.setBounds(605, 126, 132, 37);
 		btn_DangNhap.setHeSoBoGoc(10);
 		contentPane.add(btn_DangNhap);
 	}
+
 	public JPanel getTimKiem_View() {
 		return contentPane;
 	}
