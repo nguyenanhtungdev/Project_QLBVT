00b71e468a5a62b67391bfeafd8ac904d5617052
Phùng Văn Tú <tuphung.03032004@gmail.com>
2024-11-25 08:14:19 +0700

    Cập nhật Thẻ thống kê
    - Đổi tên TheThongKe thành TrainStatisticCard
    - Thêm documentation
    
    Signed-off-by: Phùng Văn Tú <tuphung.03032004@gmail.com>

--- src/main/java/other/TrainStatisticCard.java
+++ src/main/java/other/TrainStatisticCard.java
@@ -1,7 +1,6 @@
 package other;
 
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
@@ -12,30 +11,36 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TheThongKe extends JPanel {
+import constant.FontConstants;
+
+public class TrainStatisticCard extends JPanel {
 
 	private static final long serialVersionUID = -2454984566733070396L;
 
 	private JLabel lName, lValue;
 	private int arc = 16;
 
-	public TheThongKe(String name, String value) {
-		lName = new JLabel(name);
-		lValue = new JLabel(value);
-
-		lName.setFont(new Font(null, Font.PLAIN, 24));
-		lValue.setFont(new Font(null, Font.BOLD, 40));
-
-		lName.setForeground(ColorConstants.TEXT_COLOR);
-		lValue.setForeground(ColorConstants.TEXT_COLOR);
-
-		add(lName);
-		add(Box.createGlue());
-		add(lValue);
-
+	/**
+	 * Tạo một thẻ thống kê với tên và giá trị
+	 * 
+	 * @param name  Tên thống kê
+	 * @param value Giá trị thống kê
+	 */
+	public TrainStatisticCard(String name, String value) {
 		setOpaque(false);
 		setPreferredSize(new Dimension(300, 125));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		add(lName = new JLabel(name));
+		add(Box.createGlue());
+		add(lValue = new JLabel(value));
+
+		lName.setFont(FontConstants.HEADING_5);
+		lValue.setFont(FontConstants.HEADING_2B);
+
+		lName.setForeground(ColorConstants.TEXT_COLOR);
+		lValue.setForeground(ColorConstants.SECONDARY_COLOR); // ACCENT was used in the original version
+
 	}
 
 	public void setNameText(String name) {
@@ -57,7 +62,7 @@ public class TheThongKe extends JPanel {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.setColor(ColorConstants.PRIMARY_COLOR);
-		g2d.fillRoundRect(0, 0, getWidth() - 4, getHeight() - 4, arc, arc);
+		g2d.fillRoundRect(0, 0, getWidth(), getHeight(), arc, arc);
 	}
 
 }
