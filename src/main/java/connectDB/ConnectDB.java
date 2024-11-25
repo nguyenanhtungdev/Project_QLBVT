f363fe5eb4733d966222d50e42fd0dc64706ac8b
Phùng Văn Tú <tuphung.03032004@gmail.com>
2024-11-25 07:44:52 +0700

    Cải thiện ConnectDB
    - Thêm documentation
    - Thay đổi thứ tự hàm và biến
    - Thay đổi biến static connectDB từ public thành private
    
    Signed-off-by: Phùng Văn Tú <tuphung.03032004@gmail.com>

--- src/main/java/connectDB/ConnectDB.java
+++ src/main/java/connectDB/ConnectDB.java
@@ -6,7 +6,56 @@ import java.sql.SQLException;
 
 public class ConnectDB {
 
-	public static ConnectDB connectDB;
+	private static ConnectDB connectDB;
+	private final String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyBanVeTau";
+	private String user;
+	private String password;
+	private Connection connection;
+
+	/**
+	 * Tạo một instance đến database với password được lấy từ biến môi trường
+	 */
+	public ConnectDB() {
+		this.user = "sa";
+		this.password = System.getenv("MSSQL_PASSWORD");
+	}
+
+	/**
+	 * Tạo một instance đến database với user và password được truyền vào
+	 * 
+	 * @param user     người dùng
+	 * @param password mật khẩu
+	 */
+	public ConnectDB(String user, String password) {
+		this.user = user;
+		this.password = password;
+	}
+
+	/**
+	 * Get connection to the database<br/>
+	 * Việc đống kết nối nên được thực hiện <strong>khi và chỉ khi</strong> kết thúc
+	 * chương trình để đảm bảo tài nguyên
+	 * 
+	 * @return Connection to the database
+	 * @throws SQLException if a database access error occurs
+	 */
+	public Connection getConnection() throws SQLException {
+		if (connection == null || connection.isClosed())
+			connection = DriverManager.getConnection(url, user, password);
+		return connection;
+	}
+
+	/**
+	 * Đóng kết nối đến database<br/>
+	 * Việc đống kết nối nên được thực hiện <strong>khi và chỉ khi</strong> kết thúc
+	 * chương trình để đảm bảo tài nguyên
+	 * 
+	 * @throws SQLException if a database access error occurs
+	 */
+	public void disconnect() throws SQLException {
+		if (connection != null)
+			connection.close();
+	}
 
 	public static ConnectDB getInstance() {
 		if (connectDB == null)
@@ -14,23 +63,4 @@ public class ConnectDB {
 		return connectDB;
 	}
 
-	private final String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyBanVeTau";
-	private final String user = "sa";
-	private String password;
-	private Connection connection;
-
-	public ConnectDB() {
-		password = System.getenv("MSSQL_PASSWORD");
-	}
-
-	public Connection getConnection() throws SQLException {
-		if (connection == null || connection.isClosed())
-			connection = DriverManager.getConnection(url, user, password);
-		return connection;
-	}
-
-	public void disconnect() throws SQLException {
-		if (connection != null)
-			connection.close();
-	}
 }
